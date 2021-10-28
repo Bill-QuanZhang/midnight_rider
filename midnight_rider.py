08e578fd8899d8224d908db46ce5f92f3451fb72
Bill Zhang <20030612zq@gmail.com>
2021-10-28 11:49:18 -0700

    completed def typewriter_effect and changed def introduction

--- midnight_rider.py
+++ midnight_rider.py
@@ -14,10 +14,14 @@ class Game:
     """
     def introduction(self) -> None:
         """Print the introduction text"""
-        print(midnight_rider_text.INTRODUCTION)
+        self.typewriter_effect(midnight_rider_text.INTRODUCTION)
 
     def typewriter_effect(self, text: str) -> None:
         """Print out to console with a typewriter effect"""
+        for char in textwrap.dedent(text):
+            time.sleep(0.05)
+            sys.stdout.write(char)
+            sys.stdout.flush()
 
 
 def main() -> None:
