be68580598fe203d820e32008973d7f32d49b648
Bill Zhang <20030612zq@gmail.com>
2021-10-28 11:18:50 -0700

    created class Game and some def

--- midnight_rider.py
+++ midnight_rider.py
@@ -3,3 +3,18 @@
 # A text-based game of intrigue and illusion
 
 
+class Game:
+    """Represent our game engine
+
+    """
+    def introduction(self) -> None:
+        """Print the introduction text"""
+        #
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
