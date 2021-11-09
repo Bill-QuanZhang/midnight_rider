7d4d719a04704b6e4864e3a1c3ac0b0e47c1b0f5
Bill Zhang <20030612zq@gmail.com>
2021-11-09 09:08:11 -0800

    complete points

--- pygame_drawing.py
+++ pygame_drawing.py
@@ -17,9 +17,36 @@ BLUE = (0, 0, 255)
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+WINDOW_TITLE = "Pygame Drawing"
+
 
 def main() -> None:
     """Driver of Python script"""
+    # Create the screen
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption(WINDOW_TITLE)
+
+    # Create some local variables that describe the environment
+    done = False
+    clock = pygame.time.Clock()
+
+    # Create the main loop
+    while not done:
+        # Make space for the event listener
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        # Change the environment
+
+        # Draw the environment
+        screen.fill(WHITE)      # fill with background color
+
+        # Update the screen
+        pygame.display.flip()
+
+        # Tick the clock
+        clock.tick(60)
 
 
 if __name__ == "__maim__":
